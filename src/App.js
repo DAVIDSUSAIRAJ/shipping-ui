9725f9ba408bc55e0dd3908cda2c41c3f9bf5823
Davidsusairaj s <davidsusairaj.sebasthiyar@solverminds.com>
2024-09-10 22:32:06 +0530

    intial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import Button from "@mui/material/Button";
+import doubleHeadArrow from "./images/doubleHeadArrow.png";
 
-function App() {
+const options = ["Option 1", "Option 2"];
+
+export function App() {
+  const [value, setValue] = React.useState(options[0]);
+  const [inputValue, setInputValue] = React.useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Autocomplete
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+          inputValue={inputValue}
+          onInputChange={(event, newInputValue) => {
+            setInputValue(newInputValue);
+          }}
+          id="controllable-states-demo"
+          options={options}
+          sx={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Controllable" />
+          )}
+        />
+
+        <img
+          src={doubleHeadArrow}
+          width={25}
+          height={25}
+          alt="Double Head Arrow"
+        />
+        <Autocomplete
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+          inputValue={inputValue}
+          onInputChange={(event, newInputValue) => {
+            setInputValue(newInputValue);
+          }}
+          id="controllable-states-demo"
+          options={options}
+          sx={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Controllable" />
+          )}
+        />
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer components={["DatePicker"]}>
+            <DatePicker
+              label="From Date"
+              sx={{
+                width: 300,
+                fontSize: 18,
+                color: "blue",
+                textAlign: "center",
+              }}
+              format="DD-MM-YYYY"
+            />
+          </DemoContainer>
+        </LocalizationProvider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer components={["DatePicker"]}>
+            <DatePicker
+              label="From Date"
+              sx={{
+                width: 300,
+                fontSize: 18,
+                color: "blue",
+                textAlign: "center",
+              }}
+              format="DD-MM-YYYY"
+            />
+          </DemoContainer>
+        </LocalizationProvider> */}
+    <TextField
+  className="input_txt_label"
+  fullWidth
+  id="departure"
+  label="Departure Date"
+  type="datetime-local"
+  size="small"
+  variant="outlined"
+  InputLabelProps={{ shrink: true }}
+/>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Button variant="contained" sx={{ width: 300 }}>
+          Contained
+        </Button>
+      </div>
     </div>
   );
 }
